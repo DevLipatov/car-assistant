eb27cf83275ed1e29a094716984187d9fa799908
Mavios <yarolsav.lipatov.90@gmail.com>
2016-10-25 14:07:44 +0200

    flavors

--- app/src/main/java/com/main/carassistant/http/WeatherHttpClient.java
+++ app/src/main/java/com/main/carassistant/http/WeatherHttpClient.java
@@ -1,5 +1,7 @@
 package com.main.carassistant.http;
 
+import com.main.carassistant.BuildConfig;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,7 +10,7 @@ import java.net.URL;
 
 public class WeatherHttpClient {
 
-    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+//    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
     private static String APIKEY = "&APPID=c3859f38427daf4b538322c8c539736c";
     private static String PARAMS = "&units=metric";
 
@@ -18,7 +20,7 @@ public class WeatherHttpClient {
         String response = null;
 
         try {
-            URL url = new URL(BASE_URL + cityID + PARAMS + APIKEY);
+            URL url = new URL(BuildConfig.WEATHER_URL + cityID + PARAMS + APIKEY);
             httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod("GET");
             httpURLConnection.connect();
